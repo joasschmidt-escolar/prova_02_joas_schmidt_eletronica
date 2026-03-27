157be3d7a907ff42583c75e41c36f3bf3946cada
joasschmidt-escolar <joas_schmidt@estudante.sesisenai.org.br>
2026-03-27 20:05:40 -0300

    unidade de medida

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let continua = true;
 
 while(continua){
-    let escolha = parseInt(prompt("Digite\n[0] para sair\n[1] para conversão de unidades\n[2] para cálculo de resistores\n[3] para calculo de consumo de energia elétrica\n[4] para lei de ohm\n"));
+    let escolha = parseInt(prompt("Digite\n[0] para sair\n[1] para conversão de unidades\n[2] para cálculo de resistores\n[3] para calculo de consumo de energia elétrica\n[4] para lei de ohm\n[5] para Req de resistores"));
 
     switch(escolha){
         case 0: 
@@ -20,6 +20,7 @@ while(continua){
         case 4:
             leiDeOhm();
             break;
- 
+        case 5:
+            reqDeResistores();
     }
 }
---
+++ reqDeResistores.js
@@ -0,0 +1,22 @@
+function reqDeResistores(){
+    let resistores = parseInt(prompt("Digite:\n[1] para resistores em série\n[2] para resistores em paralelo:"));
+    let numResistores = parseInt(prompt("Digite o número de registores:"));
+    let req, serie = 0, paralelo = 0, valorDeResis;
+
+    switch(resistores){
+        case 1:
+            for(let cont = 0; cont < numResistores; cont++){
+                valorDeResis = parseInt(prompt("Digite o valor do registor em ohms: "))
+                serie +=  valorDeResis;
+            }
+            alert(serie)
+            break;
+        case 2:
+            for(let cont = 0; cont < numResistores; cont++){
+                valorDeResis = parseInt(prompt("Digite o valor do registor em ohms: "))
+                paralelo +=  1/valorDeResis;
+            }
+            alert(paralelo)
+            break; 
+    }
+}
